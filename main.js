8e752755e9ad73ddbfbd6cdb33cf2cbbab82a758
Mauro Molinari <mauro.molinari@knobs.it>
2019-11-28 11:18:56 +0100

    Added buttons to our project

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 Vue.component('navbar', {
-    props: ['title'],
+    props: {
+        title: String
+    },
     template: "<h3>{{title}}</h3>",
+})
+
+Vue.component('app-view', {
+    template: "<div><slot></slot></div>"
+})
+
+Vue.component('app-component', {
+    template: "<div><slot>CONTENT</slot></div>"
+})
+
+Vue.component('toggle-button', {
+    template: "<button>Toggle</button>"
+})
+
+Vue.component('counter-button', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: "<button @click='count++'>You clicked {{count}} times.</button>"
 })
 
 var app = new Vue({
